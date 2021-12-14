290a30e25009ef956c7d4389fe6e3d11bc762778
Yeming Gu <51205903090@stu.ecnu.edu.cn>
2021-12-14 19:22:19 +0800

    feat:优化代码逻辑，并且增加了对不存在文件的请求处理
    Signed-off-by: Yeming Gu <51205903090@stu.ecnu.edu.cn>

--- src/pages/index/datePicker.js
+++ src/pages/index/datePicker.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DatePicker, TimePicker, Select, Space, Button,message } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 
@@ -8,32 +8,15 @@ function PickerWithType({ type, onChange }) {
   return <DatePicker picker={type} onChange={onChange} />;
 }
 
-const solveDate = (year,month)=>{
-    if(year===null&&month===null){
-        return "not found";
-    }
-    if(month===null){
-        return year+"年";
-    }
-    return year+"年"+(month+1)+"月";
-}
-function DateTitle(props){
-    console.log(props);
-    return (
-        <h1>{solveDate(props.year,props.month)}</h1>
-    );
-}
-
 function SwitchablePicker(props) {
   // 默认初始为 ‘month’
   const [type, setType] = useState('month');
   const [year, setYear] = useState(null);
   const [month, setMonth] = useState(null);
-  const [nowDate, setNowDate] = useState({year:props.year,month:props.month});
 
   return (
     <>
-        <Space style={{'marginBottom':'20px'}}>
+        <Space style={{'marginBottom':'50px'}}>
             <Select value={type} onChange={setType}>
                 <Option value="month">Month</Option>
                 <Option value="year">Year</Option>
@@ -58,11 +41,9 @@ function SwitchablePicker(props) {
                     message.error('This is an error date');
                     return false;
                 }
-                setNowDate({year:year,month:month});
-                props.fetch(year,month);
+                props.update(year,month);
             }}>Search</Button>
         </Space>
-        <DateTitle year={nowDate.year} month={nowDate.month} />
     </>
   );
 }
--- src/pages/index/table.js
+++ src/pages/index/table.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import { Table, Tag, Space } from 'antd';
-import qs from 'qs';
+import { message, Table } from 'antd';
 import 'antd/dist/antd.css';
 import { toJSON } from '../../util/csv';
 import SwitchablePicker from './datePicker';
+
+const solveDate = (year,month)=>{
+    if(year===null&&month===null){
+        return "not found";
+    }
+    if(month===null){
+        return year+"年";
+    }
+    return year+"年"+(month+1)+"月";
+}
+
+function DateTitle(props){
+    return (
+        <h1>{solveDate(props.year,props.month)}</h1>
+    );
+}
 
 const columns = [
     {
         title: 'Rank',
         dataIndex: 'rank',
-        sorter: (a,b)=> a.rank - b.rank,
+        sorter: (a, b) => a.rank - b.rank,
         width: '5%',
     },
     {
@@ -34,27 +48,36 @@ const columns = [
 
 class MyTable extends React.Component {
     state = {
+        flag:false, // 为 true 的话，componentDidUpdate函数中会请求新数据
         data: [],
         pagination: {
             current: 1,
             pageSize: 10,
         },
         loading: false,
-        url:'',
-        base:"https://xlab-open-source.oss-cn-beijing.aliyuncs.com/open_index/activity/company/",
-        year:null,// 字符串格式
-        month:null,// 整数格式，0表示1月，1表示2月...
+        url: '',
+        base: "https://xlab-open-source.oss-cn-beijing.aliyuncs.com/open_index/activity/company/",
+        year: null,// 字符串格式
+        month: null,// 整数格式，0表示1月，1表示2月...
     };
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state.year = props.year;
         this.state.month = props.month;
-        this.state.url =  this.state.base+props.year+(props.month+1)+'.csv';
+        this.state.url = this.state.base + props.year + (props.month + 1) + '.csv';
     }
 
     componentDidMount() {
-        const { pagination,url } = this.state;
-        this.fetch({ pagination,url});
+        const { pagination, url } = this.state;
+        this.fetch({ pagination, url });
+    }
+
+    componentDidUpdate() {
+        if(this.state.flag==true){
+            this.setState({flag:false});
+            const { pagination, url } = this.state;
+            this.fetch({ pagination, url });
+        }
     }
 
     // 每次排序、查看下一页其实都是重新请求文件，然后按要求渲染
@@ -69,21 +92,41 @@ class MyTable extends React.Component {
         });
     };
 
-    updateDate = (year,month)=>{
-        this.state.year=year;
-        this.state.month=month;
-        this.state.url=this.state.base+year+(month+1)+'.csv';
-        const { pagination,url } = this.state;
-        this.fetch({pagination,url});
+    // 更新 state 后自动调用 生命周期 DidUpdate 请求最新数据
+    updateDate = (year, month) => {
+        if (month === null) {
+            this.setState({
+                year: year,
+                month: month,
+                url: this.state.base + year + '.csv',
+                flag:true,
+            });
+        }
+        else {
+            this.setState({
+                year: year,
+                month: month,
+                url: this.state.base + year + (month + 1) + '.csv',
+                flag:true,
+            });
+        }
     }
 
     fetch = (params = {}) => {
-        console.log(params);
         this.setState({ loading: true });
+        console.log(params.url);
         fetch(params.url)
-            .then(res => res.text())
+            .then(res => {
+                // Todo：最好的情况是在日期选择器中，只显示可以查询的日期，
+                if(res.status==404){
+                    message.warning('No relevant results yet');
+                    return '';
+                }
+                return res.text();
+            })
             .then(data => {
                 data = toJSON(data);
+                console.log(data);
                 this.setState({
                     loading: false,
                     data: data,
@@ -92,14 +135,18 @@ class MyTable extends React.Component {
                         total: data.length,
                     },
                 });
-            });
+            })
+            .catch(err => {
+                console.log('hi!' + err);
+            })
     };
 
     render() {
         const { data, pagination, loading, year, month } = this.state;
         return (
             <>
-                <SwitchablePicker year={year} month={month} fetch={this.updateDate}/>
+                <SwitchablePicker update={this.updateDate} />
+                <DateTitle year={year} month={month} />
                 <Table
                     columns={columns}
                     rowKey={record => record.rank}
@@ -109,7 +156,7 @@ class MyTable extends React.Component {
                     onChange={this.handleTableChange}
                 />
             </>
-            
+
         );
     }
 }
