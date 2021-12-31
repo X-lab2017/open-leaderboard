f043162f4d896082b699d181c199bf34978e1c4c
Yang Ming <51205903076@stu.ecnu.edu.cn>
2021-12-31 14:30:41 +0800

    Update i18n.js

--- src/util/i18n.js
+++ src/util/i18n.js
@@ -56,8 +56,8 @@ i18n
           },
           glossary:'GLOSSARY',
           Glossary:{
-            activity:'',
-            influence:'',
+            activity:'Activity is an algorithm based on the analysis method of historical behavior log data, which counts the behavior of all warehouses and developers of GitHub in the whole domain, and weights the behavior data of GitHub. The specific calculation method is: ad = ∑ wiciad = ∑ wici, where ADAD is the developer activity, and CICI is the occurrence times of the above five behavior events triggered by the developer, Wiwi is the weighted proportion of the behavior event. According to a simple value evaluation, we can set this value to 1 - 5, that is, 1 point for issue comment, 2 points for issue, 3 points for PR, 4 points for code review comment on PR, and 5 points for pr. After the activity of each developer is calculated, the activity of the project can be calculated by a weighted sum method. The previously given method is: ar = ∑ ad − − √ ar = ∑ ad, that is, the activity of the project is the sum of the activity of all developers. Here, the square is to reduce the impact of the excessive activity of core developers. (project activity only considers the behavior data of developers in the current project.) for more information about activity, please check how to evaluate an open source project (I) - activity blog.',
+            influence:'On the basis of activity, a new upper layer algorithm is designed, which is a project influence evaluation method based on the global developer cooperation network. While evaluating the cooperation influence of all projects at one time, it can also deeply explore the cooperation relevance of the project and automatically judge the category of the project, So as to obtain the network cooperation influence of projects and individuals. For more information about influence, please see how to evaluate an open source project (2) - influence blog.',
             bot:''
           }
       },
@@ -100,8 +100,8 @@ i18n
             },
             glossary:'词典',
             Glossary:{
-              activity:'',
-              influence:'',
+              activity:'活跃度是基于历史行为日志数据的分析方法，在全域范围内统计 GitHub 所有仓库和开发者的行为，并对 GitHub 行为数据的加权的算法，具体的计算方式为：Ad=∑wiciAd=∑wici 其中的 AdAd 为开发者活跃度，而 cici 为上述五种行为事件由该开发者触发的发生次数，wiwi 为该行为事件的加权比例。按照一个简单的价值评判，我们可以将这个值设置为 1 - 5，即 Issue 评论每个计 1 分、发起 Issue 每个计 2 分、发起 PR 每个计 3 分、PR 上的代码 review 评论每个计 4 分、PR 合入一个计 5 分。在计算出每个开发者的活跃度后，可以通过一种加权和的方式来计算项目的活跃度，之前给出的方式是：Ar=∑Ad−−−√Ar=∑Ad 即项目的活跃度为所有开发者活跃度的开方和，这里开方是为了降低核心开发者过高的活跃度带来的影响。（项目的活跃度只考虑开发者在当前项目中的行为数据。）想要了解更多有关活跃度的信息，请查看如何评价一个开源项目（一）——活跃度这篇博客。',
+              influence:'在活跃度的基础上设计了一种新的上层算法，一种基于全域开发者协作网络的项目影响力评估方法，在一次性评估出所有项目的协作影响力的同时，也可以对项目的协作关联度进行深入探索，并对项目的所属类别进行自动判断，从而得出项目和个人的网络协作影响力。想要了解更多有关影响力的信息，请查看如何评价一个开源项目（二）——影响力这篇博客。',
               bot:''
             }
           }
