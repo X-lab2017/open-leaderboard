54ab7f05f50c0f63489f9bc36470ab649888c944
Yeming Gu <yeminggood@gmail.com>
2022-03-28 17:45:06 +0800

    fix: responsive description
    Signed-off-by: Yeming Gu <yeminggood@gmail.com>

--- src/Description.js
+++ src/Description.js
@@ -1,43 +1,38 @@
-import {Row, Col, Image, Divider} from 'antd';
+import {Row, Col, Image, Divider, Space} from 'antd';
 import { useTranslation } from 'react-i18next';
 
 const Description = () => {
     const {t} = useTranslation();
     return (
         <>
-            <Row style={{marginTop:50,minHeight:'100px'}} align='middle' justify='center' gutter={10}>
+            <Row align='middle' justify='center' style={{margin:'50px auto',lineHeight:'1.2'}}>
                 <Col>
-                    <span className='myFontColor myTitle'>
+                    <span className='myFontColor myTitle' style={{margin:'0 auto',textAlign:'center'}}>
                         An insight to the world of 
                     </span>
+                    <span className='specialColor myTitle' style={{margin:'0 auto',textAlign:'center'}}>
+                        {' '}open source
+                    </span>
+                </Col>
+            </Row>
+            <Row style={{maxWidth:'1000px',margin:'0 auto'}}>
+                <Col>
+                    <span className='myFontColor' style={{margin:'0 auto'}}>
+                        欢迎来到 OpenLeaderboard，我们对 GitHub 上的企业、项目以及开发者账号进行了活跃度和影响力的排名。
+                        （GitHub 是全球最大的社交编程及代码托管网站。）
+                    </span>
+                </Col>
+            </Row>
+            <Row style={{maxWidth:'1000px',margin:'20px auto'}} gutter={4}>
+                <Col>
+                    <img src='/pics/Ellipse BG.png'/>
                 </Col>
                 <Col>
-                    <span className='specialColor myTitle'>
-                        open source
-                    </span>
-                </Col>
-            </Row>
-            <Row style={{minHeight:'100px'}} align='middle' justify='center'>
-                <Col span={12}>
                     <span className='myFontColor'>
-                    欢迎来到 OpenLeaderboard，我们对 GitHub 上的企业、项目以及开发者账号进行了活跃度和影响力的排名。
-                    （GitHub 是全球最大的社交编程及代码托管网站。）
+                        2022 年 2 月更新
                     </span>
                 </Col>
-            </Row>
-            <Row style={{minHeight:'100px'}} align='middle' justify='center' gutter={10}>
-                <Col span={12}>
-                    <Row gutter={10}>
-                        <Col>
-                            <img src='/pics/Ellipse BG.png'/>
-                        </Col>
-                        <Col>
-                            <span className='myFontColor'>
-                                2022 年 2 月更新
-                            </span>
-                        </Col>
-                    </Row>
-                </Col>
+                
             </Row>
         </>
     )
