63c3ce317b617df9b48ffda9457eaf4a6f0efd74
Huang <763230412@qq.com>
2023-02-22 10:34:46 +0800

    fix: switching language problem

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,7 @@ const { Header } = Layout;
 
 const MyHeader = () => {
   const { t } = useTranslation();
-  const [language, setLanguage] = useState('zh_CN');
+  const [language, setLanguage] = useState('zh');
   const changeLanguage = () => {
     let lan = language;
     if (language == 'zh') {
