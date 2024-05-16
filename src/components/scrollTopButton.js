847c4ccbeb2d8ff90c31d7e9d1a40e14e234e7a4
Apurv Sonawane <apurvsonawane28@gmail.com>
2024-05-16 16:59:40 +0530

    Revert "Fixed scroll to top"
    This reverts commit 5b3dca5bddc45379acf230957f6ecd0d81df28b1.

--- src/components/scrollTopButton.js
+++ src/components/scrollTopButton.js
@@ -1,48 +1,31 @@
-import { useState, useEffect } from 'react';
 import { Button } from 'antd';
 import { VerticalAlignTopOutlined } from '@ant-design/icons';
 function scrollToTop(timestamp) {
+  let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
   window.scrollTo({
     top: 0,
     left: 0,
     behavior: 'smooth',
   });
 }
-const ScrollTopButton = () => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      setIsVisible(scrollY > 200);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+const ScrollTopButton = (props) => {
   return (
-    <>
-      {isVisible && (
-        <Button
-          style={{
-            position: 'fixed',
-            zIndex: 2147483640,
-            right: '22px',
-            bottom: '60px',
-            backgroundColor: '#FFCC19',
-            borderColor: '#FFCC19',
-          }}
-          type="primary"
-          icon={<VerticalAlignTopOutlined />}
-          onClick={scrollToTop}
-        />
-      )}
-    </>
+    <Button
+      style={{
+        position: 'fixed',
+        zIndex: 2147483640,
+        right: '22px',
+        bottom: '60px',
+        backgroundColor: '#FFCC19',
+        borderColor: '#FFCC19',
+      }}
+      type="primary"
+      icon={<VerticalAlignTopOutlined />}
+      onClick={() => {
+        console.log('hi');
+        window.requestAnimationFrame(scrollToTop);
+      }}
+    />
   );
 };
-
 export default ScrollTopButton;
