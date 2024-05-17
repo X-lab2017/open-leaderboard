5817d8ba6a9fffabe7b2db4f57105a79e663f493
Apurv428 <84929607+Apurv428@users.noreply.github.com>
2024-05-17 10:30:08 +0530

    fix: set the display time of the scroll top button
    * Fixed scroll to top
    
    Signed-off-by: Apurv Sonawane <apurvsonawane28@gmail.com>
    
    * Revert "Fixed scroll to top"
    
    This reverts commit 5b3dca5bddc45379acf230957f6ecd0d81df28b1.
    
    * Fixed scroll to top
    
    Signed-off-by: Apurv Sonawane <apurvsonawane28@gmail.com>
    
    * Formatted with prettier
    
    Signed-off-by: Apurv Sonawane <apurvsonawane28@gmail.com>
    
    * added prop
    
    Signed-off-by: Apurv Sonawane <apurvsonawane28@gmail.com>
    
    * chore: run yarn prettier
    
    ---------
    
    Signed-off-by: Apurv Sonawane <apurvsonawane28@gmail.com>
    Co-authored-by: AndyHuang <763230412@qq.com>

--- src/components/scrollTopButton.js
+++ src/components/scrollTopButton.js
@@ -1,7 +1,7 @@
+import { useState, useEffect } from 'react';
 import { Button } from 'antd';
 import { VerticalAlignTopOutlined } from '@ant-design/icons';
 function scrollToTop(timestamp) {
-  let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
   window.scrollTo({
     top: 0,
     left: 0,
@@ -9,23 +9,38 @@ function scrollToTop(timestamp) {
   });
 }
 const ScrollTopButton = (props) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setIsVisible(scrollY > 200);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
-    <Button
-      style={{
-        position: 'fixed',
-        zIndex: 2147483640,
-        right: '22px',
-        bottom: '60px',
-        backgroundColor: '#FFCC19',
-        borderColor: '#FFCC19',
-      }}
-      type="primary"
-      icon={<VerticalAlignTopOutlined />}
-      onClick={() => {
-        console.log('hi');
-        window.requestAnimationFrame(scrollToTop);
-      }}
-    />
+    <>
+      {isVisible && (
+        <Button
+          style={{
+            position: 'fixed',
+            zIndex: 2147483640,
+            right: '22px',
+            bottom: '60px',
+            backgroundColor: '#FFCC19',
+            borderColor: '#FFCC19',
+          }}
+          type="primary"
+          icon={<VerticalAlignTopOutlined />}
+          onClick={scrollToTop}
+        />
+      )}
+    </>
   );
 };
 export default ScrollTopButton;
